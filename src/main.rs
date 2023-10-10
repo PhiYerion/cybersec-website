e33c2277dba330864249454db372b90aab8a2074
PhiYerion <reidbandy@gmail.com>
2023-10-10 13:47:45 -0400

    extensibility

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,24 @@ fn create_cursor<'a>() -> ReadSignal<&'a str> {
     cursor
 }
 
-
 #[component]
-fn Header() -> impl IntoView {
-    const HEADER: &str = "CYBER SECURITY CLUB";
-    const header_len: usize = HEADER.len();
+fn TypedText(text: &'static str) -> impl IntoView {
+    let text_len: usize = text.len();
 
-
-    let (text, set_text)                    = create_signal(String::new());
+    let (text_render, set_text_render)      = create_signal(String::new());
     let (index, set_index)                  = create_signal(0usize);
     let (text_finished, set_text_finished)  = create_signal(false);
 
     let duration: u64 = 275;
-    let time_to_run: u64 = (header_len as u64 * (duration + 1) ).try_into().unwrap();
+    let time_to_run: u64 = (text_len as u64 * (duration + 1) ).try_into().unwrap();
 
     let typer = set_interval_with_handle (move || {
 
-        if index() >= header_len { return };
+        if index() >= text_len { return };
         
-        set_text.update(|text| 
-            (*text).push(
-                HEADER.chars().nth(index()).unwrap()
+        set_text_render.update(|s| 
+            (*s).push(
+                text.chars().nth(index()).unwrap()
         ));
 
         set_index.update(|idx| *idx += 1);
@@ -49,14 +46,21 @@ fn Header() -> impl IntoView {
     let cursor = create_cursor();
     
     view! {
+      // padding for when text is being made, but reserve cool effect
+      // when text is drawn.
+      <span class="invisible">
+        {move || if text_finished() {""} else {cursor()}}
+      </span>
+      {text_render}{cursor}
+    }
+}
+
+#[component]
+fn Header() -> impl IntoView {
+    view! {
       <nav class="text-3xl flex justify-center items-center w-screen pt-12">
         <h1 class="hackerfont text-center text-green-500">
-          // padding for when text is being made, but reserve cool effect
-          // when text is drawn.
-          <span class="invisible">
-            {move || if text_finished() {""} else {cursor()}}
-          </span>
-          {text}{cursor}
+          <TypedText text="ETSU CYBERSECURITY CLUB"/>
         </h1>
       </nav>
     }
