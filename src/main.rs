01c58cab28c738853d77b508960b693b26a774d5
PhiYerion <reidbandy@gmail.com>
2023-10-11 02:10:11 -0400

    better sections

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,100 @@
-use std::{time::Duration};
-use leptos::*;
+use leptos::{*};
+
 mod typing;
+
 use typing::TypedText;
 
 
 #[component]
 fn Header() -> impl IntoView {
     view! {
-      <nav class="text-3xl flex justify-center items-center w-screen pt-12">
+      <nav style="background-color: #222" class="text-4xl flex justify-center items-center w-screen pt-12 pb-6">
         <h1 class="hackerfont text-center text-green-500">
-          <TypedText text="ETSU CYBERSECURITY CLUB"/>
+          <TypedText centered=true interval=275 stop=false text="ETSU CYBERSECURITY CLUB" />
         </h1>
       </nav>
     }
 }
 
+struct Section {
+  header: &'static str,
+  text: &'static str,
+  delay: u64,
+}
+
 #[component]
-fn Body(
+fn TypedSection(base: Section) -> impl IntoView {
+    let header_interval = 70;
+    let text_interval = 7;
+
+    view! {
+      <div class="px-6">
+        <h2 class="text-lg">
+          <TypedText 
+            text={base.header} 
+            delay={base.delay} 
+            interval={header_interval} 
+            centered=false 
+            stop=true 
+          />
+        </h2>
+        <TypedText 
+          text={base.text} 
+          delay={base.delay + base.header.len() as u64 * header_interval + 20} 
+          interval={text_interval} 
+          centered=false 
+          stop=true 
+        />
+      </div>
+    }
+}
+
+#[component]
+fn Aboutus() -> impl IntoView {
+    let cmd_text = "cat README.md";
+    let cmd_interval = 4;
+    let hero_delay = cmd_text.len() as u64 * cmd_interval;
+
+    let sectionOne = Section {
+        header: "test header",
+        text: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+        delay: 89,
+    };
+    let sectionTwo = Section {
+        header: "test header two",
+        text: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+        delay: sectionOne.header.len() as u64 * 70 
+             + sectionOne.text.len() as u64 * 7
+             + sectionOne.delay + 20,
+    };
+
+    view! {
+      <div class="px-8 text-gray-200">
+        <p class="hackerfont text-lg">
+          <span class="text-red-500">
+            "[root@csc.etsu.edu /docs/aboutus]# "
+          </span>
+          <span>
+            <TypedText centered=false interval={cmd_interval} stop=true text={cmd_text} />
+          </span>
+        </p>
+        <div class="flex hackerfont text-gray-200 text-base px-8">
+          <TypedSection base=sectionOne/>
+          <TypedSection base=sectionTwo/>
+        </div>
+      </div>
+    }
+}
+
+
+#[component]
+fn Main(
     children: Children
 ) -> impl IntoView {
     view! {
-      <body style="background-color: #111">
+      <div>
         {children()}
-      </body>
+      </div>
     }
 }
 
@@ -30,9 +102,10 @@ fn Body(
 #[component]
 fn App() -> impl IntoView {
     view! {
-        <Body>
+        <Main>
           <Header/>
-        </Body>
+          <Aboutus/>
+        </Main>
     }
 }
 
--- src/typing.rs
+++ src/typing.rs
@@ -1,57 +1,71 @@
 use std::{time::Duration};
-use leptos::*;
+use leptos::{*, leptos_dom::helpers::IntervalHandle};
 
 
-fn create_cursor<'a>() -> ReadSignal<&'a str> {
+fn create_cursor<'a>() -> (ReadSignal<&'a str>, WriteSignal<&'a str>, IntervalHandle) {
     let (cursor, set_cursor) = create_signal("I");
 
-    set_interval(move || {
+    let handler = set_interval_with_handle(move || {
         set_cursor.update(|x| {
         if *x == "|"    {*x = " "}
         else            {*x = "|"} 
         })
     }, Duration::from_millis(400));
 
-    cursor
+    (cursor, set_cursor, handler.unwrap())
 }
 
 #[component]
-pub fn TypedText(text: &'static str) -> impl IntoView {
+pub fn TypedText(
+    text: &'static str, 
+    interval: u64, 
+    #[prop(default = false)]
+    centered: bool, 
+    #[prop(default = false)]
+    stop: bool,
+    #[prop(default = 0)]
+    delay: u64) -> impl IntoView {
     let text_len: usize = text.len();
 
     let (text_render, set_text_render)      = create_signal(String::new());
     let (index, set_index)                  = create_signal(0usize);
     let (text_finished, set_text_finished)  = create_signal(false);
-
-    let duration: u64 = 275;
-    let time_to_run: u64 = (text_len as u64 * (duration + 1) ).try_into().unwrap();
-
-    let typer = set_interval_with_handle (move || {
-
-        if index() >= text_len { return };
-        
-        set_text_render.update(|s| 
-            (*s).push(
-                text.chars().nth(index()).unwrap()
-        ));
-
-        set_index.update(|idx| *idx += 1);
-    }, Duration::from_millis(duration));
-
+    let (cursor, set_cursor, cursor_handle) = create_cursor();
+    
+    let time_to_run: u64 = (text_len as u64 * (interval + 1) ).try_into().unwrap();
+    
     set_timeout(move || {
-        typer.unwrap().clear();
-        set_text_finished.update(|x| *x = true);
-    }, Duration::from_millis(time_to_run));
+        let typer = set_interval_with_handle (move || {
 
-    let cursor = create_cursor();
+            if index() >= text_len { return };
+            
+            set_text_render.update(|s| 
+                (*s).push(
+                    text.chars().nth(index()).unwrap()
+            ));
+
+            set_index.update(|idx| *idx += 1);
+        }, Duration::from_millis(interval));
+        
+
+        set_timeout(move || {
+            typer.unwrap().clear();
+            set_text_finished.update(|x| *x = true);
+            if stop { cursor_handle.clear(); set_cursor.update(|x| *x = "") }
+        }, Duration::from_millis(time_to_run));
+    }, Duration::from_millis(delay));
+
     
     view! {
       // padding for when text is being made, but reserve cool effect
       // when text is drawn.
       <span class="invisible">
-        {move || if text_finished() {""} else {cursor()}}
+        {move || if (text_finished() || !centered) {""} else {cursor()}}
       </span>
-      {text_render}{cursor}
+      {text_render}
+      <span class="text-gray-300">
+        {cursor}
+      </span>
     }
 }
 
